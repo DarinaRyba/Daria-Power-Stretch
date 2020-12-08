f57810591307ee6e96415b8d84ed2f6d78383421
Darina Rybalchenko <dari.ryb@gmail.com>
2020-12-08 21:39:59 +0100

    fixed login as independent component

---
+++ frontend/src/components/Header/Login.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import './Header.css';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
+import { signInWithGoogle, signOut } from '../../redux/actions/auth-actions';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+function Login({ dispatch }) {
+  const [user, setUser] = useState(null);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem('user')));
+  }, []);
+
+  const handleLogin = () => {
+    dispatch(signInWithGoogle());
+  };
+
+  const handleLogout = () => {
+    dispatch(signOut());
+  };
+
+  return (
+
+    <>
+      <div className={classes.root}>
+        {!user?.uid
+          ? <Button onClick={() => handleLogin()} id="btn-login" className="header__btn-login" variant="contained">Login</Button>
+          : <Button onClick={() => handleLogout()} id="btn-logout" className="header__btn-login" variant="contained">Logout</Button>}
+      </div>
+      <div className={classes.root}>
+        <Avatar alt="" src={user?.photoURL} />
+      </div>
+    </>
+
+  );
+}
+
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  user: PropTypes.shape({}),
+};
+
+Login.defaultProps = {
+  user: { displayName: null },
+};
+
+function mapStateToProps(state) {
+  return {
+    user: state.usersReducer.user,
+    isLogged: state.usersReducer.isLogged,
+  };
+}
+
+export default connect(mapStateToProps)(Login);
