c370130ba22eea945a8b41848b879ae3a639b312
Darina Rybalchenko <dari.ryb@gmail.com>
2020-12-10 12:29:00 +0100

    putMethid in userController created

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,4 +1,4 @@
-function usersController (userSchema) {
+function usersController (userSchema, scheduleSchema) {
   function getUserMethod (req, res) {
     const query = {};
     userSchema.find(query, (usersError, user) => {
@@ -19,8 +19,29 @@ function usersController (userSchema) {
     });
   }
 
+  function putUserMethod ({ body }, res) {
+    const query = { _id: body.user._id };
+    userSchema.findOne(query, (userError, user) => {
+      if (userError) {
+        res.send(userError);
+      } else {
+        user.days.push(body.days._id);
+        user.save();
+      }
+    });
+    const queryFound = { _id: body.days._id };
+    scheduleSchema.findOne(queryFound, (daysError, days) => {
+      if (daysError) {
+        res.send(daysError);
+      } else {
+        days.participants.push(body.participants._id);
+        days.save();
+      }
+    });
+  }
+
   return {
-    getUserMethod, patchUserMethod
+    getUserMethod, patchUserMethod, putUserMethod
   };
 }
 
--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
 const usersController = require('./../controllers/userController');
 
-function workoutRouter (userSchema) {
+function workoutRouter (userSchema, scheduleSchema) {
   const router = Router();
-  const users = usersController(userSchema);
+  const users = usersController(userSchema, scheduleSchema);
 
   router.route('/')
     .get(users.getUserMethod)
